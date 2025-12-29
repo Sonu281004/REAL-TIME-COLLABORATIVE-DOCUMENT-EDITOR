228ce9a79d20f150a2e8a9d5c75d3c553bb0670f
Kannoujiya Ku Soni Ramesh <152206385+Sonu281004@users.noreply.github.com>
2025-12-30 04:28:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/REAL-TIME-COLLABORATIVE-DOCUMENT-EDITOR/",
 })
-
-export default {
-  base: "/REAL-TIME-COLLABORATIVE-DOCUMENT-EDITOR/"
-}
